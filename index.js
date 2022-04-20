a4a6e6f4cfaf182632ea3a5649f4c202bba2ed1f
anilktpss <anilktpss@gmail.com>
2022-04-20 16:14:35 +0530

    updated new events

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-let express = require('express');
-let app = express();
-let server = require('http').Server(app);
-let io = require('socket.io')(server, {
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
     cors: {
         origin: '*'
     }
 });
-let stream = require('./ws/stream');
-let path = require('path');
-let favicon = require('serve-favicon');
-const { Socket } = require('socket.io');
+const stream = require('./ws/stream');
+const path = require('path');
+const favicon = require('serve-favicon');
 require('dotenv').config();
 
-let PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -21,9 +20,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-
-const Io = io.of('/stream').on('connection', (socket) =>{
-    stream(Io, socket)
+io.of('/stream').on('connection', (socket) => {
+    stream(io, socket)
 });
 
 server.listen(PORT, () => console.log('Listening on PORT', PORT));
--- ws/stream.js
+++ ws/stream.js
@@ -1,23 +1,114 @@
-var activeUsers = [];
-var roomUsers = [];
-var likes = 0;
-const stream = (io, socket) => {
+let roomsList = [];
+var roomLikes = {};
 
+const stream = (io, socket) => {
 
     socket.emit("connected", "Hello and Welcome to the Server");
 
-    socket.on('join', (data) => {
-        socket.name = data.name
-        socket.userId = socket.id
-        const userData = {
-            name: data.name,
-            userId: socket.id
-        }
-        activeUsers.push(userData);
-        socket.emit("join", activeUsers);
+    socket.on('get-active-users', async () => {
+        const actUsers = []
+        const activeUsers = await io.of('/stream').allSockets()
+        activeUsers.forEach((value) => actUsers.push(value))
+        io.of('/stream').emit('get-active-users', { activeUsers: actUsers })
     })
 
-    socket.on('subscribe', (data) => {
+    socket.on('create-room', (room) => {
+        if (socket.adapter.rooms.has(room.name) === false) {
+            socket.join(room.name);
+            socket.join(room.socketId)
+            roomsList.push(room.name)
+            socket.emit('create-room', { msg: 'Room created successfully' })
+        } else {
+            socket.emit('create-room', { msg: 'This room already exist' })
+        }
+    });
+
+    socket.on('join-room', async (room) => {
+        if (socket.adapter.rooms.has(room.name) === true) {
+            socket.join(room.name);
+            socket.join(room.socketId)
+            const ids = await io.of('/stream').in(room.name).allSockets();
+            io.of('/stream').to(room.name).emit('join-room', {
+                msg: `New user joined id: ${socket.id}`,
+                totalUsers: ids.size,
+                userId: socket.id
+            })
+        } else {
+            socket.emit('join-room', {
+                msg: `This room is not exist`,
+                totalUsers: 0,
+                userId: ''
+            })
+        }
+    });
+
+    socket.on('leave-room', async (room) => {
+        if (socket.adapter.rooms.has(room.name) === true) {
+            socket.leave(room.name);
+            const ids = await io.of('/stream').in(room.name).allSockets();
+            io.of('/stream').to(room.name).emit('leave-room', {
+                msg: `User left id: ${socket.id}`,
+                totalUsers: ids.size,
+                userId: socket.id
+            })
+        } else {
+            socket.emit('leave-room', { msg: 'Something went wrong' })
+        }
+    });
+
+    socket.on('get-rooms-list', () => {
+        socket.emit('get-rooms-list', { rooms: roomsList })
+    })
+
+    socket.on('get-rooms-users', async (room) => {
+        const ids = await io.of('/stream').in(room.name).allSockets();
+        socket.emit('get-rooms-users', { users: ids.size })
+    })
+
+    socket.on('delete-room', async (room) => {
+        if (socket.adapter.rooms.has(room.name) === true) {
+            io.of('/stream').to(room.name).emit('delete-room', {
+                msg: `Room deleted: ${room.name}`
+            })
+            io.of('/stream').in(room.name).socketsLeave(room.name);
+            var index = roomsList.indexOf(room.name)
+            roomsList.splice(index, 1)
+            delete roomLikes[room.name]
+        } else {
+            socket.emit('leave-room', { msg: 'room not exist' })
+        }
+    })
+
+    socket.on('room-chat', (chat) => {
+        socket.to(chat.room).emit('chat', { sender: chat.sender, msg: chat.msg });
+    });
+
+    socket.on('room-likes', (room) => {
+        if (socket.adapter.rooms.has(room.name) === true) {
+            roomLikes[room.name] = roomLikes[room.name] ? roomLikes[room.name] + 1 : 1
+            io.of('/stream').to(room.name).emit('room-likes', roomLikes[room.name])
+        } else {
+            socket.emit('room-likes', `${room.name} not exist`)
+        }
+    })
+
+    socket.on('room-link', (room) => {
+        if (socket.adapter.rooms.has(room.name) === true) {
+            io.of('/stream').to(room.socketId).emit('room-link', { roomName: room.name, sender: room.sender })
+        } else {
+            socket.emit('room-link', { msg: `${room.name} not exist` })
+        }
+    })
+
+    socket.on("disconnect", () => {
+        io.of('/stream').emit('disconnect-user', { msg: 'User disconnected', userId: socket.id })
+    });
+
+
+
+
+
+    socket.on('subscribe', async (data) => {
         //subscribe/join a room
         socket.join(data.room);
         socket.join(data.socketId);
@@ -26,7 +117,8 @@ const stream = (io, socket) => {
             userId: data.socketId
         }
         roomUsers.push(roomData);
-        io.emit('subscribeResponse', { msg: 'room created successfully', count: roomUsers.length })
+        const ids = await io.of('/stream').in(data.room).allSockets();
+        io.of('/stream').to(data.room).emit('subscribeResponse', { msg: 'room created successfully', count: ids.size })
         //Inform other members in the room of new user's arrival
         if (socket.adapter.rooms.has(data.room) === true) {
             socket.to(data.room).emit('new user', { socketId: data.socketId });
@@ -59,22 +151,14 @@ const stream = (io, socket) => {
         socket.to(data.to).emit('ice candidates', { candidate: data.candidate, sender: data.sender });
     });
 
-
     socket.on('chat', (data) => {
         socket.to(data.room).emit('chat', { sender: data.sender, msg: data.msg });
     });
 
     socket.on('likes', (data) => {
-        likes= likes+1
+        likes = likes + 1
         io.emit('likes', likes)
     })
-
-    socket.on("disconnect", () => {
-        activeUsers.splice(activeUsers.findIndex(function (i) {
-            return i.userId === socket.userId;
-        }), 1);
-        socket.emit("user-disconnected", activeUsers);
-    });
 };
 
 module.exports = stream;
